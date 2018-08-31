6d9bc831c02d9b242533211d63b36144a26e62a2
Ian Marshall <i>
2018-08-31 17:21:22 +0100

    Section 5, Lecture 37

--- DataAnnotations/Course.cs
+++ DataAnnotations/Course.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAnnotations
 {
@@ -11,15 +13,23 @@ namespace DataAnnotations
 
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
 
+        [Required]
+        [MaxLength(2000)]
         public string Description { get; set; }
 
         public int Level { get; set; }
 
         public float FullPrice { get; set; }
 
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
         public virtual Author Author { get; set; }
+
         public virtual ICollection<Tag> Tags { get; set; }
     }
 }
